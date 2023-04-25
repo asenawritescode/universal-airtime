c4fcf8e87923ed22b0d70bf1281e15cd821a8dc5
asenawritescode <samasena2001@gmail.com>
2023-04-25 15:11:19 +0300

    Algorithm Complete, pending functions

--- feistal-cipher.py
+++ feistal-cipher.py
@@ -1,10 +1,12 @@
-data = "123456789"
+# data = "123456789"
+data = "123455559"
 
 # divide the data into two parts using slicing 
 left, right = data[:len(data)//2], data[len(data)//2:]
 print("Start: /n")
 print("Left: ", left)
 print("Right: ", right)
+print("End: ")
 
 # switch them up 
 def switch(left, right):
@@ -18,6 +20,11 @@ def switch(left, right):
 
 def  hash1(n):
     # transposing the string
+
+    return n
+
+def  hash2(n):
+    # shift the string
     return n
 
 # function round 1
@@ -29,22 +36,22 @@ def round1(left, right):
     # copy right to left
     left_temp = right
     # XOR right_h and left
-    right = right_h ^ left
-    left = left_temp
+    new_right = right_h ^ left
+    new_left = left_temp
     
     # return left and right, call function round2
-    return round2(left, right)
+    return round2(new_left, new_right)
 
-
-def round2(left, right):
+def round2(new_left, new_right):
     # right gets into function
     
-    right_h = hash1(right)
+    right_h = hash2(new_right)
     # copy right to left
-    left_temp = right
+    left_temp = new_right
     # XOR right_h and left
-    right = right_h ^ left
+    right = right_h ^ new_left
     left = left_temp    
+    # return print("Left: ", left, "Right: ", right)
     return switch(left, right)
 
 

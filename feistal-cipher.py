d828e9bc6c187004a50594f3647010c572465f78
asenawritescode <samasena2001@gmail.com>
2023-04-25 17:01:32 +0300

    test new hashing functions

--- feistal-cipher.py
+++ feistal-cipher.py
@@ -1,5 +1,6 @@
 # data = "123456789"
-data = "123455559"
+# data = "123455559"
+data = "32921780431"
 
 # divide the data into two parts using slicing 
 left, right = data[:len(data)//2], data[len(data)//2:]
@@ -18,13 +19,22 @@ def switch(left, right):
 
     return data
 
-def  hash1(n):
-    # transposing the string
+def swap(code, pos1, pos2): 
+    # convert string to list of characters 
+    code = list(str(code)) 
+    # swap the position of the characters 
+    code[pos1], code[pos2] = code[pos2], code[pos1] 
+    # convert list to string 
+    return ''.join(code)
 
+def  hash1(n):
+    # swap string positions
+    n = swap(n, 0, 2)
     return n
 
 def  hash2(n):
-    # shift the string
+    # swap string positions
+    n = swap(n, 1, (len(n)-1))
     return n
 
 # function round 1
@@ -32,7 +42,7 @@ def round1(left, right):
     left = int(left)
     right = int(right)
     # right gets into function
-    right_h = hash1(right)
+    right_h = int(hash1(right))
     # copy right to left
     left_temp = right
     # XOR right_h and left
@@ -45,7 +55,7 @@ def round1(left, right):
 def round2(new_left, new_right):
     # right gets into function
     
-    right_h = hash2(new_right)
+    right_h = int(hash2(str(new_right)))
     # copy right to left
     left_temp = new_right
     # XOR right_h and left

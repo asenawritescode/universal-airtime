c26c96f9b4ea17183cdfa6f5c38b85d202b80214
asenawritescode <samasena2001@gmail.com>
2023-05-06 18:21:47 +0300

    Refactor code to OOP

--- GenerateVoucher.py
+++ GenerateVoucher.py
@@ -1,34 +1,113 @@
 from nanoid import generate as g
 
-# Amounts from 10 - 1000  
+class Voucher:
+    """
+    Voucher Generation Implementation
 
-def add_value(a , v):
-    n = int(str(len(a)))
-    i = n - 1
-    for d in a:
-        # Add the amount to the voucher
-        pos = pow(2, i)
-        v[pos] = d
-        i-=1
-    v[15] = n # assign amount value to the position 15 
+    Attributes:
+    ----------
+    amount: int
+        The value of the voucher
+    code: str
+        The voucher code
+    
+    Methods:
+    -------
+    gen_voucher(amount):
+        Generates a voucher code of the specified value
+    
+    addValue(amount, voucher):
+        Embed voucher value to the geneated voucher code
+    
+    """
 
-    return ''.join(map(str, v))
+    def __init__(self, amount):
+        """
+        Parameters:
+        ----------
+        amount: int
+            The value of the voucher
+        """
 
-def gen_voucher(amount):
-   # check if amount is valid 
-   if amount < 10 or amount > 1000:
-        print("Invalid amount")
-        return
+        self.amount = amount
+        self.code = self.gen_voucher(amount)
 
-   # Generate a random 12 digit number
-   code = g("0123456789", 16)
-   # print(code, "code")
-   # Convert to a list of integers
-   v_str = list(str(code))
-   a_str = list(str(amount))
-   # Iterate
-   v_digits = [int(digit) for digit in v_str]
-   a_digits = [int(digit) for digit in a_str]
+    def __str__(self):
+        """
+        Returns:
+        -------
+        code : str
+        """
 
-   final = add_value(a_digits, v_digits)
-   return final
+        return self.code.__str__()
+    
+    def __repr__(self) -> str:
+        """
+        Returns:
+        -------
+        Voucher object : str
+        """
+
+        return 'Voucher(' + self.amount +','+ self.code +')'
+    
+    def add_value(self, voucher: int, amount: int) -> str:
+        """
+        Embed voucher value to the geneated voucher code
+
+        Parameters:
+        ----------
+        voucher: int
+            The voucher code
+        amount: int
+            The value of the voucher
+        
+        Returns:
+        ------
+        voucher : str
+        """
+        n = int(str(len(amount)))
+        i = n - 1
+        for d in amount:
+            pos = pow(2, i)
+            voucher[pos] = d # Embed the amount to the voucher
+            i-=1
+        voucher[15] = n # assign amount value len at position 15
+        
+        voucher = ''.join(map(str, voucher))
+        
+        return voucher
+    
+    def gen_voucher(self, amount: int) -> str:
+        """
+        Generates a voucher code of the specified value
+
+        Parameters:
+        ----------
+        amount: int
+            The value of the voucher
+        
+        Raises:
+        ------
+
+        
+        Returns:
+        -------
+        code : str
+        """
+
+        if amount < 10 or amount > 1000: # check if amount is in valid range (10-1000)
+            raise ValueError("Invalid amount ("+amount.__str__()+") should be between 10 and 1000")
+
+        code = g("0123456789", 16) # Generate a random 16 digit number
+        v_str = list(code) 
+        a_str = list(amount.__str__()) 
+        v_digits = [int(digit) for digit in v_str] 
+        a_digits = [int(digit) for digit in a_str] 
+
+        voucher = self.add_value(v_digits, a_digits) 
+
+        return voucher
+
+a = Voucher(100)
+
+print(a)
